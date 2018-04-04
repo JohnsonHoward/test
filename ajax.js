f7da47635662311098083e856655e86ca50e42c7
jhoward <jhoward@adaptavist.com>
2018-04-04 12:32:14 +0100

    Dashboard Test

--- ajax.js
+++ ajax.js
@@ -11,7 +11,7 @@
             alert("Sorry. Server unavailable. ");
           });
 
-
+$('form').submit(function(event) {
 function refresh() {
 jQuery.ajax({
  type: "GET",
@@ -25,3 +25,4 @@ jQuery.ajax({
                    alert("Sorry. Server unavailable. ");
 });
 }
+});
